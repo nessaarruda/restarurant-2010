41612e255e778deb97d12548ef5ac7d1366537a0
Vanessa Arruda <wanessaarruda@gmail.com>
2020-10-07 13:44:57 -0600

    Add test for menu_dish_name method

--- test/restaurant_test.rb
+++ test/restaurant_test.rb
@@ -57,5 +57,16 @@ class RestaurantTest < Minitest::Test
     restaurant2 = Restaurant.new('16:00', 'Il Poggio')
     assert restaurant1.open_for_lunch?
     refute restaurant2.open_for_lunch?
-  end 
+  end
+
+  def test_if_list_of_dishes_is_all_caps
+    restaurant1 = Restaurant.new('10:00', 'Fuel Cafe')
+    restaurant2 = Restaurant.new('16:00', 'Il Poggio')
+
+    restaurant2.add_dish('Burrata')
+    restaurant2.add_dish('Pizzetta')
+    restaurant2.add_dish('Ravioli')
+
+    assert_equal ["BURRATA", "PIZZETTA", "RAVIOLI"], restaurant2.menu_dish_names
+  end
 end
